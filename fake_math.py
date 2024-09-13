2aad6905152da6a0d1c5e4d54963f0cd5f3fe89e
DmitriyEliseev <93958489+DmitriyEliseev@users.noreply.github.com>
2024-09-13 15:40:36 +0300

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -1,9 +1,5 @@
-def division(first, second):
+def divide(first,second):
     if second == 0:
         return 'Ошибка'
-    return first / second
-
-result1 = division(69, 3)
-result2 = division(3, 0)
-print(result1)
-print(result2)
+    else:
+        return first / second
