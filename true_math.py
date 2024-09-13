eb5d74f405c3bb13ddb958f102357912c1be704b
DmitriyEliseev <93958489+DmitriyEliseev@users.noreply.github.com>
2024-09-13 15:41:12 +0300

    Update true_math.py

--- true_math.py
+++ true_math.py
@@ -1,12 +1,8 @@
 from math import inf
 
-def divide (first, second):
+
+def divide(first, second):
     if second == 0:
-         return inf
-
-    return first / second
-
-result3 = divide (49, 7)
-result4 = divide (15, 0)
-print(result3)
-print(result4)
+        return inf
+    else:
+        return first / second
